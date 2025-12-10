a848e5a05810ffe7f70d914fc620bac52e1b48fa
xy23d <ypinku@yahoo.co.jp>
2025-12-10 17:26:33 +0900

    feat: AC

--- answers/abc234_c.go
+++ answers/abc234_c.go
@@ -1,11 +1,32 @@
-// 問題文
-// 10 進法で表記したときに 
-// 0,2 のみからなる正整数のうち、 
-// K 番目に小さいものを求めてください。
+package main
 
-// 制約
-// K は 
-// 1 以上 
-// 10 
-// 18
-//   以下の整数
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+
+	var k int64
+	fmt.Fscan(in, &k)
+
+	digits := make([]byte, 0, 64)
+	for k > 0 {
+		if k%2 == 0 {
+			digits = append(digits, '0')
+		} else {
+			digits = append(digits, '2')
+		}
+		k /= 2
+	}
+
+	for i, j := 0, len(digits)-1; i < j; i, j = i+1, j-1 {
+		digits[i], digits[j] = digits[j], digits[i]
+	}
+
+	fmt.Fprintln(out, string(digits))
+}
