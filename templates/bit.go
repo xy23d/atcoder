5d246938ce83af222addaf4553fba11a7f847ced
xy23d <33027925+xy23d@users.noreply.github.com>
2026-01-15 16:29:19 +0900

    update: bit (#54)

--- templates/bit.go
+++ templates/bit.go
@@ -5,8 +5,12 @@ import (
 )
 
 func main() {
-	fmt.Println(1 << 0) // 1
-	fmt.Println(1 << 1) // 2
-	fmt.Println(3 & 1 << 1) // 2
-	fmt.Println(3 | 1 << 1) // 3
+	fmt.Println(1 << 0) // 1　左シフト
+	fmt.Println(1 << 1) // 2 左シフト
+	fmt.Println(3 & 1 << 1) // 2 アンド
+	fmt.Println(3 | 1 << 1) // 3 OR
+	fmt.Println(3 ^ 1 << 1) // 1 XOR
+	fmt.Println((3>>2)&1 == 1) // そのbitに1がたっているかどうか
+	fmt.Println(3^(1<<2)) // そのbitの1→0
+	fmt.Println(3|(1<<2)) // そのbitに1をたてる
 }
